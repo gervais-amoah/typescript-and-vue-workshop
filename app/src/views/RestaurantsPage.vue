2b26402da357c473881120f075b8862dd4a420cb
gervais <gervaisamoah@yahoo.com>
2024-07-12 20:18:48 +0000

    ok

--- app/src/views/RestaurantsPage.vue
+++ app/src/views/RestaurantsPage.vue
@@ -1,15 +1,23 @@
-<script>
+<script lang="ts">
+import type { Restaurant } from '@/types'
 import NewRestaurantForm from '../components/NewRestaurantForm.vue'
 import RestaurantCard from '../components/RestaurantCard.vue'
 import SideMenu from '../components/SideMenu.vue'
+import { defineComponent } from 'vue'
 
-export default {
+type DataShape = {
+  filterText: string
+  restaurantList: Restaurant[]
+  showNewForm: boolean
+}
+
+export default defineComponent({
   components: {
     NewRestaurantForm,
     RestaurantCard,
     SideMenu,
   },
-  data: () => ({
+  data: (): DataShape => ({
     filterText: '',
     restaurantList: [
       {
@@ -71,7 +79,7 @@ export default {
       showNewForm.value = true
     }
   },
-}
+})
 </script>
 
 <template>
